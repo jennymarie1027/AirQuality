d1b4e7462049bf9f9ae9fdb1c4df05cce13e9c63
Jenny McGrath <jennifermcgrath@Jennys-MacBook-Pro.local>
2024-12-27 10:48:45 -0700

    connect to airnow apit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
   def index
     require 'net/http'
     require 'json'
-    @url = 'https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=25&API_KEY=B3CDAE6C-4CC1-4D86-8A95-371B28332224'
+    @url = 'https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=80521&distance=25&API_KEY=B3CDAE6C-4CC1-4D86-8A95-371B28332224'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @output = JSON.parse(@response)
+
   end
 end
