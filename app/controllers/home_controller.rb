dbd8b89c50d34812166bc8e5ad52f2bc942cd32b
Jenny McGrath <jennifermcgrath@Jennys-MacBook-Pro.local>
2024-12-27 10:39:16 -0700

    install json

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    require 'net/http'
+    require 'json'
+    @url = 'https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=25&API_KEY=B3CDAE6C-4CC1-4D86-8A95-371B28332224'
   end
 end
